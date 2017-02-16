5d795f5b58aa3e9e8b44b0feb43075579c7e6265
Renat <Renat@IDEA-PC>
2017-02-16 18:59:56 +0100

    Added properties into business entity car

--- CarRental.Business.Entities/Car.cs
+++ CarRental.Business.Entities/Car.cs
@@ -7,8 +7,25 @@ using System.Threading.Tasks;
 
 namespace CarRental.Business.Entities
 {
+    [DataContract]
     public class Car
     {
+        [DataMember]
+        public int CarId { get; set; }
 
+        [DataMember]
+        public string Description { get; set; }
+
+        [DataMember]
+        public string Color { get; set; }
+
+        [DataMember]
+        public int Year { get; set; }
+
+        [DataMember]
+        public decimal RentalPrice { get; set; }
+
+        [DataMember]
+        public bool CurrentlyRented { get; set; }
     }
 }
