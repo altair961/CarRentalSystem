601df21a6000293ff22c6f54aa5668898e3b5693
Renat <Renat@IDEA-PC>
2017-02-18 13:25:39 +0100

    Added Account, Rental and Reservation business entities

---
+++ CarRental/CarRental.Business.Entities/Rental.cs
@@ -0,0 +1,40 @@
+﻿using Core.Common.Contracts;
+using Core.Common.Core;
+using System;
+using System.Runtime.Serialization;
+
+namespace CarRental.Business.Entities
+{
+    [DataContract]
+    public class Rental : EntityBase, IIdentifiableEntity, IAccountOwnedEntity
+    {
+        [DataMember]
+        public int RentalId { get; set; }
+
+        [DataMember]
+        public int AccountId { get; set; }
+
+        [DataMember]
+        public int CarId { get; set; }
+
+        [DataMember]
+        public DateTime DateRented { get; set; }
+
+        [DataMember]
+        public DateTime DateDue { get; set; }
+
+        [DataMember]
+        public DateTime? DateReturned { get; set; }
+
+        public int EntityId
+        {
+            get { return RentalId; }
+            set { RentalId = value; }
+        }
+
+        int IAccountOwnedEntity.OwnerAccountId
+        {
+            get { return AccountId; }
+        }
+    }
+}
